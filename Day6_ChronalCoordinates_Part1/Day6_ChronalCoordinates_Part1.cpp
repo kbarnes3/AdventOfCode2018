ae39d64a4772faa4bd94de7cb29ebb1eddc7c25a
Kevin Barnes <kbarnes3@gmail.com>
2018-12-10 09:55:52 -0800

    6.1 sample is solved

--- Day6_ChronalCoordinates_Part1/Day6_ChronalCoordinates_Part1.cpp
+++ Day6_ChronalCoordinates_Part1/Day6_ChronalCoordinates_Part1.cpp
@@ -5,6 +5,30 @@
 #include <iostream>
 #include "Data.h"
 
+size_t distance(_In_ size_t x, _In_ size_t y, _In_ Coordinate& coordinate)
+{
+    size_t distance = 0;
+    if (coordinate.X > x)
+    {
+        distance = coordinate.X - x;
+    }
+    else
+    {
+        distance = x - coordinate.X;
+    }
+
+    if (coordinate.Y > y)
+    {
+        distance += coordinate.Y - y;
+    }
+    else
+    {
+        distance += y - coordinate.Y;
+    }
+
+    return distance;
+}
+
 template<size_t Size>
 void solve(_In_ std::array<Coordinate, Size>& coordinates)
 {
@@ -22,14 +46,79 @@ void solve(_In_ std::array<Coordinate, Size>& coordinates)
         max_y = std::max(min_y, coordinate.Y);
     }
 
-    // Then look at points within these bounds and see which coordinate it is closest too
+    // Next mark any coordinates along these bounds as having an infinite area
+    for (Coordinate& coordinate : coordinates)
+    {
+        if (coordinate.X == min_x || coordinate.X == max_x ||
+            coordinate.Y == min_y || coordinate.Y == max_y)
+        {
+            coordinate.Infinite = true;
+        }
+    }
+
+    // Then look at points within these bounds and see which coordinate it is closest to
     for (size_t x = min_x + 1; x < max_x; x++)
     {
         for (size_t y = min_y + 1; y < max_y; y++)
         {
+            typename std::array<Coordinate, Size>::iterator iter = coordinates.begin();
+            typename std::array<Coordinate, Size>::iterator closest = iter;
+            size_t closestDistance = distance(x, y, *iter);
+            iter++;
 
+            while (iter != coordinates.end())
+            {
+                size_t iterDistance = distance(x, y, *iter);
+                // If the new distance is strictly less than the old one, we have a new candidate
+                if (iterDistance < closestDistance)
+                {
+                    closest = iter;
+                    closestDistance = iterDistance;
+                }
+                else if (iterDistance == closestDistance)
+                {
+                    // If the new distance is the same, mark this node as tied for now
+                    closest = coordinates.end();
+                }
+
+                iter++;
+            }
+
+            // If there was one node that was strictly closer than all the others, increment its count of closest spaces
+            if (closest != coordinates.end())
+            {
+                closest->ClosestArea++;
+            }
         }
     }
+
+    // Finally find the coordinate with the largest finite closest area
+    typename std::array<Coordinate, Size>::const_iterator iter = std::max_element(
+        coordinates.cbegin(),
+        coordinates.cend(),
+        [](
+            _In_ const Coordinate& a,
+            _In_ const Coordinate& b
+            ) -> bool
+        {
+            // All Infinite spaces are considered equally low elements
+            if (a.Infinite && b.Infinite)
+            {
+                return false;
+            }
+            if (a.Infinite)
+            {
+                return true;
+            }
+            if (b.Infinite)
+            {
+                return false;
+            }
+            return a.ClosestArea < b.ClosestArea;
+        }
+    );
+
+        std::wcout << iter->ClosestArea << std::endl;
 }
 
 int main()
