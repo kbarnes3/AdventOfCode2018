a01c9b30d66c941d40d7d6ea950709841d8bdb27
Kevin Barnes <kbarnes3@gmail.com>
2018-12-26 09:29:45 -0800

    Starting 8.2

---
+++ Day8_MemoryManeuver_Part2/Day8_MemoryManeuver_Part2.cpp
@@ -0,0 +1,22 @@
+// Day8_MemoryManeuver_Part2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <Data.h>
+#include "NodeProcessor.h"
+
+
+template<size_t Size>
+void solve(_In_ const std::array<unsigned short, Size>& license)
+{
+    typename NodeProcessor<Size>::ProcessedNode node =
+        NodeProcessor<Size>::ProcessNode(license.cbegin());
+
+    std::wcout << node.second << std::endl;
+}
+
+int main()
+{
+    solve(inputLicense);
+}
