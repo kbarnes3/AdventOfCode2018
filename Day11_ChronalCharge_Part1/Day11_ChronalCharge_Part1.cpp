1fb6ee92e0a8330b2761811ba8727c6b0ad927aa
Kevin Barnes <kbarnes3@gmail.com>
2019-01-04 17:02:26 -0800

    Sample input works

--- Day11_ChronalCharge_Part1/Day11_ChronalCharge_Part1.cpp
+++ Day11_ChronalCharge_Part1/Day11_ChronalCharge_Part1.cpp
@@ -17,14 +17,55 @@ int computePowerLevel(int x, int y, int serialNumber)
     return powerLevel;
 }
 
+int computeTotalPower(const std::array<std::array<int, 300>, 300>& fuelCells, int x, int y)
+{
+    int power = 0;
+    for (int yOffset = y; yOffset < y + 3; yOffset++)
+    {
+        for (int xOffset = x; xOffset < x + 3; xOffset++)
+        {
+            power += fuelCells[yOffset][xOffset];
+        }
+    }
+
+    return power;
+}
+
 void solve(int serialNumber)
 {
-    int powerLevel = computePowerLevel(3, 5, serialNumber);
-    std::wcout << powerLevel << std::endl;
+    std::array<std::array<int, 300>, 300> fuelCells;
+    for (int y = 0; y < 300; y++)
+    {
+        for (int x = 0; x < 300; x++)
+        {
+            int powerLevel = computePowerLevel(x+1, y+1, serialNumber);
+            fuelCells[y][x] = powerLevel;
+        }
+    }
+
+    int maxX = 0;
+    int maxY = 0;
+    int maxPower = -100;
+
+    for (int y = 0; y < 297; y++)
+    {
+        for (int x = 0; x < 297; x++)
+        {
+            int totalPower = computeTotalPower(fuelCells, x, y);
+            if (totalPower > maxPower)
+            {
+                maxPower = totalPower;
+                maxX = x+1;
+                maxY = y+1;
+            }
+        }
+    }
+    
+    std::wcout << maxX << L',' << maxY << std::endl;
 }
 
 int main()
 {
-    solve(8);
+    solve(42);
     return 0;
 }
