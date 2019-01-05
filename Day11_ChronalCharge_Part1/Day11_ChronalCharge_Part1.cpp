b44cf2df18b3cba44d23504e328635bfdf69a1a4
Kevin Barnes <kbarnes3@gmail.com>
2019-01-04 16:36:51 -0800

    Sample power level works

--- Day11_ChronalCharge_Part1/Day11_ChronalCharge_Part1.cpp
+++ Day11_ChronalCharge_Part1/Day11_ChronalCharge_Part1.cpp
@@ -4,7 +4,27 @@
 #include "pch.h"
 #include <iostream>
 
+int computePowerLevel(int x, int y, int serialNumber)
+{
+    int rackId = x + 10;
+    int powerLevel = rackId * y;
+    powerLevel += serialNumber;
+    powerLevel *= rackId;
+    powerLevel /= 100;
+    powerLevel %= 10;
+    powerLevel -= 5;
+
+    return powerLevel;
+}
+
+void solve(int serialNumber)
+{
+    int powerLevel = computePowerLevel(3, 5, serialNumber);
+    std::wcout << powerLevel << std::endl;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    solve(8);
+    return 0;
 }
