04b25829513b9aa7e081969d73d6fe84e6cc0ae4
Kevin Barnes <kbarnes3@gmail.com>
2019-02-04 13:20:38 -0800

    Solving 12.2

--- Day12_SubterraneanSustainability_Part2/Day12_SubterraneanSustainability_Part2.cpp
+++ Day12_SubterraneanSustainability_Part2/Day12_SubterraneanSustainability_Part2.cpp
@@ -19,9 +19,9 @@ Value defaultAt(const std::map<Key, Value>& map, const Key& key, const Value& de
     }
 }
 
-std::map<int, bool> processGeneration(const std::map<int, bool>& prevGeneration, const std::map<std::tuple<bool, bool, bool, bool, bool>, bool>& rules, bool allowMissingRules)
+std::map<int64_t, bool> processGeneration(const std::map<int64_t, bool>& prevGeneration, const std::map<std::tuple<bool, bool, bool, bool, bool>, bool>& rules, bool allowMissingRules)
 {
-    std::map<int, bool> pots;
+    std::map<int64_t, bool> pots;
     // Degenerate case, an empty map produces an empty map
     if (prevGeneration.cbegin() == prevGeneration.cend())
     {
@@ -29,10 +29,10 @@ std::map<int, bool> processGeneration(const std::map<int, bool>& prevGeneration,
     }
 
     // Get the range of pots we need to consider. We need to go two left and right of the previous generation
-    int leftmost = prevGeneration.cbegin()->first - 2;
-    int rightmost = prevGeneration.crbegin()->first + 2;
+    int64_t leftmost = prevGeneration.cbegin()->first - 2;
+    int64_t rightmost = prevGeneration.crbegin()->first + 2;
 
-    for (int pot = leftmost; pot <= rightmost; ++pot)
+    for (int64_t pot = leftmost; pot <= rightmost; ++pot)
     {
         // Build up the tuple
         std::tuple<bool, bool, bool, bool, bool> nearbyPots = {
@@ -62,26 +62,11 @@ std::map<int, bool> processGeneration(const std::map<int, bool>& prevGeneration,
     return pots;
 }
 
-template<size_t Count>
-void solve(const std::array<bool, Count>& initialState, const std::map<std::tuple<bool, bool, bool, bool, bool>, bool>& rules, bool allowMissingRules)
+int64_t sumPots(const std::map<int64_t, bool>& pots)
 {
-    std::map<int, bool> pots;
-    for (int i = 0; i < Count; ++i)
-    {
-        if (initialState[i])
-        {
-            pots[i] = initialState[i];
-        }
-    }
-
-    for (unsigned int generation = 1; generation <= 20; ++generation)
-    {
-        pots = processGeneration(pots, rules, allowMissingRules);
-    }
-
-    // Take the sum of all the indices of pots that contain a plant after 20 generations
-    int sum = std::accumulate(pots.cbegin(), pots.cend(), 0,
-        [](int a, const std::pair<int, bool>& b)
+    // Take the sum of all the indices of pots that contain a plant
+    int64_t sum = std::accumulate(pots.cbegin(), pots.cend(), 0,
+        [](int64_t a, const std::pair<int64_t, bool>& b) -> int64_t
     {
         if (b.second)
         {
@@ -92,6 +77,54 @@ void solve(const std::array<bool, Count>& initialState, const std::map<std::tupl
             return a;
         }
     });
+
+    return sum;
+}
+
+template<size_t Count>
+void solve(const std::array<bool, Count>& initialState, const std::map<std::tuple<bool, bool, bool, bool, bool>, bool>& rules, bool allowMissingRules)
+{
+    std::map<int64_t, bool> pots;
+    for (int i = 0; i < Count; ++i)
+    {
+        if (initialState[i])
+        {
+            pots[i] = initialState[i];
+        }
+    }
+    
+    uint64_t generation = 1;
+    const uint64_t totalGenerations = 50000000000;
+    int64_t pastSum = sumPots(pots);
+    int64_t difference = 0;
+    int64_t differenceHits = 0;
+
+    for (; generation <= totalGenerations; ++generation)
+    {
+        pots = processGeneration(pots, rules, allowMissingRules);
+        int64_t sum = sumPots(pots);
+        int64_t newDifference = sum - pastSum;
+        pastSum = sum;
+        if (newDifference == difference)
+        {
+            ++differenceHits;
+        }
+        else
+        {
+            difference = newDifference;
+            differenceHits = 1;
+        }
+
+        if (differenceHits > 10)
+        {
+            break;
+        }
+    }
+
+    // After we find the pattern, just do the math instead of counting each generation
+    uint64_t remainingGenerations = totalGenerations - generation;
+    int64_t sum = pastSum + remainingGenerations * difference;
+
 
     std::wcout << sum << std::endl;
 }
